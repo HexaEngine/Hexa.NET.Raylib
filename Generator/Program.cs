b5a993d62831e4e12eb83c5ff18f2dca8ad1c62a
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-08-21 14:59:40 +0200

    Add project files.

---
+++ Generator/Program.cs
@@ -0,0 +1,7 @@
+﻿using HexaGen;
+using HexaGen.Patching;
+
+CsCodeGeneratorConfig config = CsCodeGeneratorConfig.Load("generator.json");
+CsCodeGenerator generator = new(config);
+generator.PatchEngine.RegisterPrePatch(new NamingPatch(["Raylib"], NamingPatchOptions.None));
+generator.Generate([.. Directory.GetFiles("include")], "../../../../Hexa.NET.Raylib/Generated");
---
+++ Hexa.NET.Raylib/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using HexaGen.Runtime;
+
+#if NET7_0_OR_GREATER
+
+using System.Runtime.CompilerServices;
+
+[assembly: DisableRuntimeMarshalling]
+#endif
+
+[assembly: NativeLibrary("raylib", TargetPlatform.Windows)]
+[assembly: NativeLibrary("libraylib", TargetPlatform.Linux)]
+[assembly: NativeLibrary("libraylib", TargetPlatform.OSX)]
+[assembly: NativeLibrary("libraylib", TargetPlatform.Android)]
